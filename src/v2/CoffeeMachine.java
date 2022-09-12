c0b7dace035b5a2307ab8319c926f4a949ededbf
loicbaillif <larmedeciel@gmail.com>
2022-09-12 21:10:13 +0200

    JaForBeg/CoffeeMach/v2/4
    Debug started on existing functions
    Next: debug exit menu

--- src/v2/CoffeeMachine.java
+++ src/v2/CoffeeMachine.java
@@ -4,6 +4,12 @@ public class CoffeeMachine {
 
 	public static void main(String[] args) {
 		System.out.println("***** Coffee Machine V2 *****");
+		String currentState = "generalMenu";
+		while (currentState != "exit") {
+			currentState = CollectUserInput.main(currentState);
+			System.out.println(currentState);
+		}
+		
 	}
 
 }
--- src/v2/CollectUserInput.java
+++ src/v2/CollectUserInput.java
@@ -13,52 +13,55 @@ public class CollectUserInput {
 		lv1State.add("take");
 		lv1State.add("remaining");
 		lv1State.add("exit");
-		*/
+		 */
 		Scanner scanner = new Scanner(System.in);
 		String userInput = "";
-		
+
 		switch (currentState) {
 		case "generalMenu":
 			userInput = generalMenu(scanner);
 			break;
-		case "buyMenu":
+		case "buy":
 			userInput = buyMenu(scanner);
 			break;
-		case "fillMenu":
+		case "fill":
 			userInput = fillMenu(scanner);
+			break;			
+		case "exit":
 			break;
 
 		default:
+			userInput = generalMenu(scanner);
 			break;
 		}
-		
+
 		return userInput;
 	}
-	
+
 	public static String generalMenu(Scanner scanner) {		
 		System.out.println("Write action (buy, fill, take, remaining, exit)");
-		String userInput = scanner.nextLine();
-		
-		return userInput;
+		return scanner.nextLine();
 	}
-	
+
 	public static String buyMenu(Scanner scanner) {
 		System.out.println("What do you want to buy? 1 - espresso, "
 				+ "2 - latte, 3 - cappuccino, back - to main menu:");
 		return scanner.nextLine();
 	}
-	
+
 	public static String fillMenu(Scanner scanner) {
 		// Variables
 		String[] ingredientList = {"ml of water", "mk of milk", 
-		                           "grams of coffee beans", "disposable cups"};
+				"grams of coffee beans", "disposable cups"};
 		String userInput = "";
 		for (int i = 0; i < ingredientList.length; i++) {
-			System.out.printf("Write how many %s you want to add:\n> ");
+			System.out.printf("Write how many %s you want to add:\n> ", ingredientList[i]);
 			userInput += scanner.nextLine();
-			userInput += ","; 
+			if (i != ingredientList.length - 1) {
+				userInput += ","; 
+			}
 		}
-		
+
 		return userInput;
 	}
 }
