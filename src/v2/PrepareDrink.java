9eb442341276f78ca6ee105fca3cf6e50f2e5c48
loicbaillif <larmedeciel@gmail.com>
2022-09-14 20:57:21 +0200

    JaForBeg/CoffeeMach/v2/11
    Stock verification to prepare requested drink only if stock is sufficient

--- src/v2/PrepareDrink.java
+++ src/v2/PrepareDrink.java
@@ -8,6 +8,15 @@ public class PrepareDrink {
 		final int[] cappuccino = new int[] {-200, -100, -12, -1, 6};
 		final int[] unknown = new int[] {0, 0, 0, 0, 0};
 		int[] drink;
+		int[] machineStock = new int[] {
+				CoffeeMachine.getWater(), 
+				CoffeeMachine.getMilk(), 
+				CoffeeMachine.getCoffeeBeans(), 
+				CoffeeMachine.getCups()
+		};
+		String[] drinks = {"unknown", "espresso", "latte", "cappuccino"}; 
+		String[] ingredients = {"water", "milk", "coffee beans", "cups"};
+		boolean drinkFeasible = false;
 
 		if (userChoice == "back") {
 			return;
@@ -30,12 +39,23 @@ public class PrepareDrink {
 			break;
 		}
 		
+		String confirmationMessage = "";
+		
+		// Ensure the machine has enough resources
+		for (int i = 0; i < machineStock.length; i++) {
+			if (machineStock[i] + drink[i] < 0) {
+				System.out.printf("Sorry, not enough %s%n", ingredients[i]);
+				return;
+			}
+		}
+		
+		// Enough stock in the machine, we can prepare the coffee
+		System.out.printf("I have enough resources, making you a %s%n", drinks[Integer.parseInt(userChoice)]);
 		CoffeeMachine.addWater(drink[0]);
 		CoffeeMachine.addMilk(drink[1]);
 		CoffeeMachine.addCoffeeBeans(drink[2]);
 		CoffeeMachine.addCups(drink[3]);
 		CoffeeMachine.addCash(drink[4]);
-		
 
 	}
 }
